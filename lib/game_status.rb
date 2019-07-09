e7a4431b0a01aa096e96d519ece8c4dba7ef4ef1
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-09 20:38:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,6 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -16,25 +17,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |combo|
-    index_0 = combo[0]
-    index_1 = combo[1]
-    index_2 = combo[2]
+  
 
-    position_1 = board[index_0]
-    position_2 = board[index_1]
-    position_3 = board[index_2]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return combo
-    end
-  end
-  return false
-end
-
-def full?(board)
-  board.all? { |position|
-  position == "X" || position == "O"}
-end
+  
+]
